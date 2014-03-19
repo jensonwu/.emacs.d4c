28aa133b310e554d04399abad6a66a714e53ebe8
Sid <sid@ubnt.com>
2014-03-19 17:26:35 +0800

    add

---
+++ init-ecb.el
@@ -0,0 +1,9 @@
+(add-to-list 'load-path "~/.emacs.d/deps/ecb")
+
+(require 'ecb)
+
+(require 'ecb-autoloads)
+
+(setq stack-trace-on-error t)
+
+(setq ecb-tip-of-the-day nil)
---
+++ init.el
@@ -0,0 +1,27 @@
+;自动加载函数
+(load-library "init-func")
+
+(load-library "init-color-moccur")
+;快捷键
+(load-library "init-shortcut")
+
+;常用配置
+(load-library "init-whitespace")
+(load-library "init-config")
+
+(load-library "init-c")
+(load-library "init-xcscope")
+(load-library "init-ecb")
+
+(load-library "init-color-theme")
+
+;目录选择
+(load-library "init-ido")
+
+(load-library "init-tabbar")
+
+;补全
+;(load-library "init-auto-complete")
+;(load-library "init-gccsense")
+
+;(load-library "init-etags")
